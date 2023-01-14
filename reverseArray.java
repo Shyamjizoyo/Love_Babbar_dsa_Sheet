fffe086930b27ce5e92695473fd1ba8100cd7f31
Shyam Ji Gupta <91778264+Shyamjizoyo@users.noreply.github.com>
2023-01-14 21:51:37 +0530

    Add files via upload

---
+++ reverseArray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;  
+public class reverseArray 
+{
+  public static void main(String [] args)
+ {
+   Scanner sc = new Scanner(System.in);
+     System.out.print(" how many numbers u want to store :: ");
+     int n = sc.nextInt();
+     System.out.println(" ");
+     int arr[] = new int[n];
+      for(int i = 0;i<n;i++)
+        {
+          arr[i] = sc.nextInt();
+        }
+         System.out.println(" the reverse array is :: ");
+         {
+          for(int i = arr.length-1;i>=0;i--)
+          {
+           System.out.print(arr[i]+" ");
+          }
+         }    
+ }
+}
